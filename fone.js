0a3801e14933059f35d57f639db3255d9d57d2e8
ArielyZX <124407945+ArielyZX@users.noreply.github.com>
2024-04-14 14:45:20 -0300

    Add files via upload

---
+++ fone.js
@@ -0,0 +1,38 @@
+btn1.addEventListener("click", function() {
+    telefone.value += btn1.value;
+});   
+btn2.addEventListener("click", function() {
+    telefone.value += btn2.value;
+});
+    btn3.addEventListener("click", function() {
+    telefone.value += btn3.value;
+});
+    btn4.addEventListener("click", function() {
+    telefone.value += btn4.value;
+});
+    btn5.addEventListener("click", function() {
+    telefone.value += btn5.value;
+});
+    btn6.addEventListener("click", function() {
+    telefone.value += btn6.value;
+});
+    btn7.addEventListener("click", function() {
+    telefone.value += btn7.value;
+});
+    btn8.addEventListener("click", function() {
+    telefone.value += btn8.value;
+});
+    btn9.addEventListener("click", function() {
+    telefone.value += btn9.value;
+});
+    btn10.addEventListener("click", function() {
+    telefone.value += btn10.value;
+
+});
+btn11.addEventListener("click", function() {
+    telefone.value += btn11.value;
+
+});   btn12.addEventListener("click", function() {
+    telefone.value += btn12.value;
+
+});   
