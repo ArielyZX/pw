63f6e876c9db2b301551ad31abae80c065749084
ArielyZX <124407945+ArielyZX@users.noreply.github.com>
2024-04-17 18:49:18 -0300

    Add files via upload

---
+++ calc.js
@@ -0,0 +1,47 @@
+btn1.addEventListener("click", function () {
+    telefone.value += btn1.value;
+});
+btn2.addEventListener("click", function () {
+    telefone.value += btn2.value;
+});
+btn3.addEventListener("click", function () {
+    telefone.value += btn3.value;
+});
+btn4.addEventListener("click", function () {
+    telefone.value += btn4.value;
+});
+btn5.addEventListener("click", function () {
+    telefone.value += btn5.value;
+});
+btn6.addEventListener("click", function () {
+    telefone.value += btn6.value;
+});
+btn7.addEventListener("click", function () {
+    telefone.value += btn7.value;
+});
+btn8.addEventListener("click", function () {
+    telefone.value += btn8.value;
+});
+btn9.addEventListener("click", function () {
+    telefone.value += btn9.value;
+});
+btn10.addEventListener("click", function () {
+    telefone.value += btn10.value;
+
+});
+btn11.addEventListener("click", function () {
+    telefone.value += btn11.value;
+
+}); btn12.addEventListener("click", function () {
+    telefone.value += btn12.value;
+
+});
+btn13.addEventListener("click", function () {
+    telefone.value += btn13.value;
+})
+btn14.addEventListener("click", function () {
+    telefone.value += btn14.value;
+})
+igual.addEventListener('click', function () {
+    telefone.value = eval(telefone.value);
+})
